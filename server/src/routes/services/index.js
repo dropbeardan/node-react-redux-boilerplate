fe4cf1f6381d2b1794cdc84baf7e0db5af8fde29
dropbeardan <dan.ess.001@gmail.com>
2018-01-20 06:24:31 +1100

    Removed obselete route for proxying.

--- server/src/routes/services/index.js
+++
@@ -1,8 +0,0 @@
-const services = require('express').Router({ mergeParams: true });
-
-const proxyRequest = require('./proxyRequest');
-
-services.route('/*')
-    .all(proxyRequest);
-
-module.exports = services;
